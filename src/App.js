edff00bb15ae09b3ef4c16e710a60345ad361079
Marothi Mahlake <code_artist@outlook.com>
2021-09-23 22:55:16 +0200

    React router and routing configuration sorted.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
-
+import "./App.css";
 function App() {
   return (
-    <div className="app">
-      <>
-        {/* Header (Nav) Component */}
-        <Header />
-        {/* Home (Hero) Component */}
-        <Home />
-      </>
-    </div>
+    <Router>
+      <div className="app">
+        <>
+          <Switch>
+            <Route path="/checkout">
+              <Header />
+              <h1>The Shopping Cart</h1>
+            </Route>
+            <Route path="/">
+              <Header />
+              <Home />
+            </Route>
+          </Switch>
+        </>
+      </div>
+    </Router>
   );
 }
 
